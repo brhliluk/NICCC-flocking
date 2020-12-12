353f9c2067771f53da9ded4d725ea8f0bcfd8f55
Ondřej Vaniš <vanisond@fit.cvut.cz>
2020-12-12 13:24:25 +0100

    Add lights

--- Demo/Assets/Scripts/Spawner.cs
+++ Demo/Assets/Scripts/Spawner.cs
@@ -6,15 +6,17 @@ public class Spawner : MonoBehaviour
 {
 
     public GameObject Streak;
-    public int spawnPeriod;
+    public float spawnPeriod;
     public Light target;
     public Light origin;
     private float time = 0;
 
+    private  GameObject[]  targets;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        targets = GameObject.FindGameObjectsWithTag("UZEMI_TARGET");
     }
 
 
@@ -22,12 +24,20 @@ public class Spawner : MonoBehaviour
     {
         time += Time.deltaTime;
 
+
         if (time >= spawnPeriod)
         {
             time = 0.0f;
 
-            GameObject sphere = Instantiate(Streak, origin.transform.position, Quaternion.identity) as GameObject;
-            sphere.GetComponent<Cannonball>().Shoot(target, Random.Range(30.0f,70.0f));
+            foreach (var targetObject in targets)
+            {
+                var target = targetObject.GetComponent<Light>();
+                if(origin.name == target.name) {
+                    continue;
+                }
+                GameObject sphere = Instantiate(Streak, origin.transform.position, Quaternion.identity) as GameObject;
+                sphere.GetComponent<Cannonball>().Shoot(target, Random.Range(30.0f,70.0f));
+            }
             // execute block of code here
         }
     }
