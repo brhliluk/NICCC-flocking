42837ba7f1baadf724c9885db229aa7a61947627
AnnaMoudra <moudrann@fit.cvut.cz>
2020-12-12 13:47:19 +0100

    Added coroutine, tags, removed colliders

--- Demo/Assets/Scripts/Spawner.cs
+++ Demo/Assets/Scripts/Spawner.cs
@@ -7,38 +7,39 @@ public class Spawner : MonoBehaviour
 
     public GameObject Streak;
     public float spawnPeriod;
-    public Light target;
     public Light origin;
-    private float time = 0;
 
     private  GameObject[]  targets;
+
+    IEnumerator Spawn(GameObject targetObject) {
+        var target = targetObject.GetComponent<Light>();
+        while (true) {
+            
+            GameObject sphere = Instantiate(Streak, origin.transform.position, Quaternion.identity) as GameObject;
+            sphere.GetComponent<Cannonball>().Shoot(target, Random.Range(30.0f, 70.0f));
+            yield return new WaitForSeconds(spawnPeriod);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         targets = GameObject.FindGameObjectsWithTag("UZEMI_TARGET");
+
+        foreach (var targetObject in targets)
+        {
+            var target = targetObject.GetComponent<Light>();
+            if (origin.name == target.name)
+            {
+                continue;
+            }
+            StartCoroutine(Spawn(targetObject));
+        }
     }
 
 
     void Update()
     {
-        time += Time.deltaTime;
 
-
-        if (time >= spawnPeriod)
-        {
-            time = 0.0f;
-
-            foreach (var targetObject in targets)
-            {
-                var target = targetObject.GetComponent<Light>();
-                if(origin.name == target.name) {
-                    continue;
-                }
-                GameObject sphere = Instantiate(Streak, origin.transform.position, Quaternion.identity) as GameObject;
-                sphere.GetComponent<Cannonball>().Shoot(target, Random.Range(30.0f,70.0f));
-            }
-            // execute block of code here
-        }
     }
 }
